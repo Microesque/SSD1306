9738edd4c2e55c960c6f8241cee5d0910d6a3db7
Microesque <microesque@gmail.com>
2024-11-11 11:24:59 +0300

    feat: implement draw_arc_fill()

--- lib/SSD1306.h
+++ lib/SSD1306.h
@@ -132,5 +132,7 @@
                                 int16_t width, int16_t height);
     void SSD1306_draw_arc(SSD1306_T* display, int16_t x0, int16_t y0, int16_t r,
                           uint8_t quadrant);
+    void SSD1306_draw_arc_fill(SSD1306_T* display, int16_t x0, int16_t y0,
+                               int16_t r, uint8_t quadrant);
 
 #endif
